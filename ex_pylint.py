18c3eca12fbc672f2b119fe80f09867af6da136c
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-07-04 07:17:23 -0300

    config: adding pylint on the project

---
+++ ex_pylint.py
@@ -0,0 +1,14 @@
+
+
+print('Hello word')
+
+
+def my_function():
+    ''' Alguma documentação sobre a função'''
+    print('Estou em minha function')
+
+
+# snake_case -> Variaveis e Funções em python
+# PascalCase - > Classes em Python
+# cammelCase -> Utilizado em JS
+
