c8a128c91ff2e1103ce632565c0c4d5fa015a93d
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-07-13 13:56:36 -0300

    feat: implementing pet_deleter_controller with tests

---
+++ src/controllers/pet_deleter_controller_test.py
@@ -0,0 +1,13 @@
+from .pet_deleter_controller import PetDeleterController
+
+def test_delete_pet(mocker):
+    # Arrange
+    mock_pets_repository = mocker.Mock()
+    pet_deleter_controller = PetDeleterController(mock_pets_repository)
+    pet_name = "Buddy"
+
+    # Act
+    pet_deleter_controller.delete(pet_name)
+
+    # Assert
+    mock_pets_repository.delete_pet.assert_called_once_with(pet_name)
