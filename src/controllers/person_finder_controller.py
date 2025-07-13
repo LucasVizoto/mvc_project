cde47334e4eed12f89864042a0b7108bfaca4ff5
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-07-13 13:24:42 -0300

    feat: implementing person_finder_controller with tests

---
+++ src/controllers/person_finder_controller.py
@@ -0,0 +1,31 @@
+from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInteface
+from src.models.sqlite.entities.people import PeopleTable
+
+class PersonFinderController:
+    def __init__(self, people_repository: PeopleRepositoryInteface) -> None:
+        self.__people_repository = people_repository
+
+    def find(self, person_id: int) -> dict:
+        person = self.__get_person_from_db(person_id)
+        formatted_response = self.__format_response(person)
+        return formatted_response
+
+    def __get_person_from_db(self, person_id: int) -> PeopleTable:
+        person = self.__people_repository.get_person(person_id)
+        if not person:
+            raise Exception("Person not found.")
+        return person
+
+    def __format_response(self, person_info: PeopleTable) -> dict:
+        return {
+            "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": {
+                    "first_name": person_info.first_name,
+                    "last_name": person_info.last_name,
+                    "pet_name": person_info.pet_name,
+                    "pet_type": person_info.pet_type
+                }
+            }
+        }
